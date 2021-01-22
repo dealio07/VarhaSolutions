1ba0967e1e6b2a708ef40c407a663a1f58ccd4fc
RegulusPrime <dealio07@gmail.com>
2021-01-22 19:25:51 +0200

    Rename

--- VarhaSolutionsCore/Program.cs
+++ VarhaSolutionsCore/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace HomeSolutionsCore
+namespace VarhaSolutionsCore
 {
     public class Program
     {
